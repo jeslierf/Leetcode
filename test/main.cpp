a2eb6cc643443534b5dd4a95de7cee5158c3be2f
jeslierf <lijwnss@gmail.com>
2021-06-06 19:51:06 -0700

    merge intervals

--- test/main.cpp
+++ test/main.cpp
@@ -2,75 +2,43 @@
 #include <vector>
 using namespace std;
 
-int main() {
-    vector<vector<char>> grid = {{'1', '0', '1', '1', '1'},{'1', '0', '1', '0', '1'}, {'1', '1', '1', '0', '1'}};
-    //vector<vector<char>> grid = {{"1","0","1","1","1"},{"1","0","1","0","1"},{"1","1","1","0","1"}};
-
-    int res = 0;
-
-    int row = grid.size();
-    int col = grid[0].size();
-
-    for (int i = 0; i < row; i++) {
-        for (int j = 0; j < col; j++) {
-            if (grid[i][j] == '1') {
-                res++;
-                vector<vector<int>> v_cur; // Discovery the first leading '1'
-                v_cur.push_back({i, j});
-                grid[i][j] = '0';
-                while (1) {
-                    cout << "#" << endl;
-                    vector<vector<int>> v_new; // Store 4 directions' coordinates
-                    for (int k = 0; k < v_cur.size(); k++) {
-                        int x = v_cur[k][0];
-                        int y = v_cur[k][1];
-
-                        // 4 directions
-                        if (x - 1 >= 0) { // UP
-                            if (grid[x - 1][y] == '1') {
-                                v_new.push_back({x - 1, y});
-                                grid[x - 1][y] = '0';
-                            }
-                        }
-                        if (x + 1 < row) { // DOWN
-                            if (grid[x + 1][y] == '1') {
-                                v_new.push_back({x + 1, y});
-                                grid[x + 1][y] = '0';
-                            }
-                        }
-                        if (y - 1 >= 0) { // LEFT
-                            if (grid[x][y - 1] == '1') {
-                                v_new.push_back({x, y - 1});
-                                grid[x][y - 1] = '0';
-                            }
-                        }
-                        if (y + 1 < col) { // RIGHT
-                            if (grid[x][y + 1] == '1') {
-                                v_new.push_back({x, y + 1});
-                                grid[x][y + 1] = '0';
-                            }
-                        }
-
-                        for (int p = 0; p < row; p ++) {
-                            for (int q = 0; q < col; q ++) {
-                                cout << grid[p][q];
-                            }
-                            cout << endl;
-                        }
-                        cout << endl;
-
-                    }
-
-                    if (v_new.size() == 0) {
-                        break;
-                    }
-                    v_cur.erase(v_cur.begin(), v_cur.end());
-                    v_cur = v_new;
-                }
+vector<vector<int>> merge(vector<vector<int>>& intervals) {
+    // Sort the intervals according to the first element
+    vector<vector<int>> si; // sorted intervals
+    si = intervals;
+    for (int i = 0; i < si.size() - 1; i++) {
+        for (int j = i + 1; j < si.size(); j++) {
+            if (si[j][0] < si[i][0]) {
+                si[i][0] = si[j][0];
+                si[i][1] = si[j][1];
             }
         }
-     }
-     cout << res << endl;
+    }
+
+    for (int i = 0; i < si.size(); i++) {
+         cout << si[i][0] << ", " << si[i][1] << endl;
+    }
+
+    vector<vector<int>> res;
+    res.push_back(si[0]);
+    for (int i = 1; i < si.size(); i++) {
+        if (si[i][0] > res[res.size() - 1][1]) {
+            res.push_back(si[i]);
+        } else if (si[i][1] > res[res.size() - 1][1]) {
+            res[res.size() - 1][1] = si[i][1];
+        }
+    }
+    return res;
+}
+
+int main() {
+    vector<vector<int>> intervals = {{1, 4}, {0, 1}};
+    vector<vector<int>> res;
+    res = merge(intervals);
+
+    for (int i = 0; i < res.size(); i++) {
+         cout << res[i][0] << ", " << res[i][1] << endl;
+    }
 
     return 0;
 }
