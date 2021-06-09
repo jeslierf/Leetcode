b36d60371de0e338c8c5a132ac90e38599591b12
jeslierf <lijwnss@gmail.com>
2021-06-09 15:43:37 -0700

    greedy

--- test/main.cpp
+++ test/main.cpp
@@ -4,32 +4,33 @@
 #include <unordered_map>
 using namespace std;
 
-bool validSub(string s, int a, int b) {
-    while(a < b) {
-        cout << s[a] << a << s[b] << b << endl;
-        if (s[a++] != s[b++])
-            return false;
-    }
-    return true;
-}
-
-bool validPalindrome(string s) {
-    for (int i = 0, j = s.size() - 1; i < j; i++, j--) {
-        if (s[i] != s[j]) {
-            return validSub(s, i + 1, j) || validSub(s, i, j - 1);
+int minMeetingRooms(vector<vector<int>>& intervals) {
+    int num = 0;
+    sort(intervals.begin(), intervals.end());
+    for (int i = 0; i < intervals.size(); i++) {
+        int j = i;
+        int local_num = 1;
+        cout << intervals[i][0] << " ";
+        while(--j >= 0) {
+            if (intervals[i][0] < intervals[j][1]) {
+                cout << intervals[j][1] << " ";
+                local_num++;
+            } else {
+                break;
+            }
         }
+        cout << endl;
+        num = local_num > num ? local_num : num;
     }
-    return true;
+
+    return num;
 }
 
 int main() {
-    string s = "deeee";
-    //bool res = validPalindrome(s);
-    //cout << res << endl;
+    vector<vector<int>> x = {{4, 18}, {1, 35}, {12, 45}, {25, 46}, {22, 27}};
 
-    cout << validSub(s, 1, 4) << endl;
-    cout << validSub(s, 0, 3) << endl;
-
+    int res = minMeetingRooms(x);
+    cout << res << endl;
 
     return 0;
 }
