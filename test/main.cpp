b27dbd5a7b0ae927f294bd2018d7f6d90c2927ec
jeslierf <lijwnss@gmail.com>
2021-06-08 20:08:39 -0700

    palindrome

--- test/main.cpp
+++ test/main.cpp
@@ -4,10 +4,32 @@
 #include <unordered_map>
 using namespace std;
 
+bool validSub(string s, int a, int b) {
+    while(a < b) {
+        cout << s[a] << a << s[b] << b << endl;
+        if (s[a++] != s[b++])
+            return false;
+    }
+    return true;
+}
+
+bool validPalindrome(string s) {
+    for (int i = 0, j = s.size() - 1; i < j; i++, j--) {
+        if (s[i] != s[j]) {
+            return validSub(s, i + 1, j) || validSub(s, i, j - 1);
+        }
+    }
+    return true;
+}
+
 int main() {
-    vector<int> x(256, 0);
-    x['a'] = 10;
-    cout << x['a'] << endl;
+    string s = "deeee";
+    //bool res = validPalindrome(s);
+    //cout << res << endl;
+
+    cout << validSub(s, 1, 4) << endl;
+    cout << validSub(s, 0, 3) << endl;
+
 
     return 0;
 }
