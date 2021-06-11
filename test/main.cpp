3e6236a4ef7e9d19ab46a06245677ddbce86188c
jeslierf <lijwnss@gmail.com>
2021-06-10 22:19:42 -0700

    ~

--- test/main.cpp
+++ test/main.cpp
@@ -9,14 +9,6 @@
 using namespace std;
 
 int main() {
-    unordered_map<int, int> m;
-    m[1] = 1;
-    m[2] = 2;
-    m[4] = 4;
-    m[3] = 3;
-
-    auto ptr = m.begin();
-    cout << ptr << endl;
 
     return 0;
 }
