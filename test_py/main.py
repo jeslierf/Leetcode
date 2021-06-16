cf410581981cb2883013dc40f9a493c0cbd4eb95
jeslierf <lijwnss@gmail.com>
2021-06-16 15:08:24 -0700

    easy linear scan

--- test_py/main.py
+++ test_py/main.py
@@ -4,26 +4,14 @@
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 
 
-def foo(logs):
-    letter_logs = []
-    digit_logs = []
-    for ele in logs:
-        temp = ele.split(" ")
-        if temp[1].isdigit():
-            digit_logs.append(ele)
-        else:
-            letter_logs.append([temp[0], " ".join(temp[1:])])
-    letter_logs = sorted(letter_logs)
-    letter_logs = sorted(letter_logs, key=lambda x: x[1])
-    return [ele[0] + " " + ele[1] for ele in letter_logs] + digit_logs
+def foo(s):
+    if s[0] == s[1]:
+        print(1)
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    l = ["j mo", "5 m w", "g 07", "o 2 0", "t q h"]
-    foo(l)
-    res = foo(l)
-    print(res)
+    foo("00")
 
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
